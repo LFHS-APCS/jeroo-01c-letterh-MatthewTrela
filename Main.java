2b21650db9270de5c3cbbaa9fbba870765c2c04f
Matthew Trela <2270347@lfschools.net>
2020-08-26 15:48:08 +0000

    Changed my Jeroo name

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main implements Directions {
      */
     public static void runJerooCode() {
        // Write code here to make a letter here
-       Jeroo kim = new Jeroo(0, 0, EAST, 100);
+       Jeroo a = new Jeroo(0, 0, EAST, 100);
        a.hop();
   a.turn(LEFT);
   a.turn(LEFT);
